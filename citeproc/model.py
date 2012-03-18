citeproc-py/citeproc-py

commit 96ed838f6475b8c97da8ba75ddf815ffbc0d411b

* support for text-case * parse nocase spans in tests

citeproc/model.py
@@ -308,21 +308,49 @@ class StrippedPeriods(object):
 
 
 class TextCased(object):
-    def case(self, text):
+    _stop_words = ['a', 'an', 'and', 'as', 'at', 'but', 'by', 'down', 'for',
+                   'from', 'in', 'into', 'nor', 'of', 'on', 'onto', 'or',
+                   'over', 'so', 'the', 'till', 'to', 'up', 'via', 'with',
+                   'yet']
+
+    def case(self, text, language=None):
         text_case = self.get('text-case')
         if text_case is not None:
+            if language != 'en' and text_case == 'title':
+                text_case = 'sentence'
             if text_case == 'lowercase':
                 text = text.lower()
             elif text_case == 'uppercase':
                 text = text.upper()
             elif text_case == 'capitalize-first':
-                text = text.capitalize()
+                text = text.capitalize_first()
             elif text_case == 'capitalize-all':
-                text = text.title()
+                output = []
+                for word in text.words():
+                    word = word.capitalize_first()
+                    output.append(word)
+                text = ' '.join(output)
             elif text_case == 'title':
-                text = text.title()
+                output = []
+                prev = ':'
+                for word in text.words():
+                    if not text.isupper() and not word.isupper():
+                        word = word.soft_lower()
+                        if (str(word) not in self._stop_words or
+                            prev in (':', '.')):
+                            word = word.capitalize_first()
+                    prev = word[-1]
+                    output.append(word)
+                text = ' '.join(output)
             elif text_case == 'sentence':
-                text = text.capitalize()
+                output = []
+                for i, word in enumerate(text.words()):
+                    if not text.isupper() and not word.isupper():
+                        word = word.soft_lower()
+                    if i == 0:
+                        word = word.capitalize_first()
+                    output.append(word)
+                text = ' '.join(output)
         return text
 
 
@@ -579,10 +607,18 @@ class Text(CitationStylesElement, Formatted, Affixed, Quoted, TextCased,
         else:
             return False
 
+    def render(self, *args, **kwargs):
+        text, language = self.process(*args, **kwargs)
+        return self.markup(text, language)
+
     def process(self, item, context=None, **kwargs):
         if context is None:
             context = self
 
+        try:
+            language = item.reference.language[:2]
+        except VariableError:
+            language = self.get_root().get('default-locale', 'en')[:2]
         if 'variable' in self.attrib:
             text = self._variable(item, context)
         elif 'macro' in self.attrib:
@@ -592,7 +628,7 @@ class Text(CitationStylesElement, Formatted, Affixed, Quoted, TextCased,
         elif 'value' in self.attrib:
             text = self.get('value')
 
-        return text
+        return text, language
 
     def _variable(self, item, context):
         variable = self.get('variable')
@@ -631,9 +667,9 @@ class Text(CitationStylesElement, Formatted, Affixed, Quoted, TextCased,
 
         return text
 
-    def markup(self, text):
+    def markup(self, text, language):
         if text:
-            tmp = self.format(self.case(self.strip_periods(text)))
+            tmp = self.format(self.case(self.strip_periods(text), language))
             return self.wrap(self.quote(tmp))
         else:
             return None
@@ -833,13 +869,13 @@ class Number(CitationStylesElement, Formatted, Affixed, Displayed, TextCased,
             variable = item.reference[variable]
 
         try:
-            first, last = map(int, self.re_range.match(variable).groups())
+            first, last = map(int, self.re_range.match(str(variable)).groups())
             first = self.format_number(first, form)
             last = self.format_number(last, form)
             text = first + chr(name2codepoint['ndash']) + last
         except AttributeError:
             try:
-                number = int(self.re_numeric.match(variable).group(1))
+                number = int(self.re_numeric.match(str(variable)).group(1))
                 text = self.format_number(number, form)
             except AttributeError:
                 text = variable

citeproc/string.py
@@ -10,6 +10,29 @@ class String(str):
     def __iadd__(self, other):
         return self.__add__(other)
 
+    def lower(self):
+        return self.__class__(super().lower())
+
+    def upper(self):
+        return self.__class__(super().upper())
+
+    def soft_lower(self):
+        return self.lower()
+
+    def soft_upper(self):
+        return self.upper()
+
+    def capitalize_first(self):
+        string = str(self)
+        result = string[0].upper()
+        if len(string) > 1:
+            result += string[1:]
+        return self.__class__(result)
+
+    def words(self):
+        for word in self.split():
+            yield self.__class__(word)
+
 
 class MixedString(list):
     def __add__(self, other):
@@ -27,16 +50,45 @@ class MixedString(list):
     def __str__(self):
         return ''.join(map(str, self))
 
+    def __getitem__(self, index):
+        return str(self)[index]
+
     def lower(self):
         return self.__class__([string.lower() for string in self])
 
     def upper(self):
         return self.__class__([string.upper() for string in self])
 
+    def title(self):
+        return self.__class__([string.title() for string in self])
+
+    def capitalize_first(self):
+        self_iter = iter(self)
+        output = [next(self_iter).capitalize_first()]
+        output += [string for string in self_iter]
+        return self.__class__(output)
+
+    def isupper(self):
+        return all(string.isupper() for string in self)
+
+    def split(self, sep=None, maxsplit=-1):
+        return str(self).split(sep, maxsplit)
+
+    def words(self):
+        for string in self:
+            for word in string.words():
+                yield word
+
 
 class NoCase(String):
     def __repr__(self):
         return '{}({})'.format(self.__class__.__name__, str(self))
 
-    def lower(self):
+    def soft_lower(self):
+        return self
+
+    def soft_upper(self):
+        return self
+
+    def capitalize_first(self):
         return self

tests/citeproc-test.py
@@ -15,6 +15,7 @@ from citeproc import CitationStylesStyle, CitationStylesBibliography
 from citeproc import NAMES, DATES, NUMBERS
 from citeproc.source import Reference, Name, Date, DateRange
 from citeproc.source import Citation, CitationItem, Locator
+from citeproc.string import String, MixedString, NoCase
 
 
 TESTS_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__),
@@ -54,6 +55,8 @@ class ProcessorTest(object):
                     value = self.parse_date(value)
                 elif python_key == 'shortTitle':
                     python_key = 'title_short'
+                else:
+                    value = self.parse_string(value)
 
                 ref_data[python_key] = value
             try:
@@ -62,6 +65,30 @@ class ProcessorTest(object):
                 # some tests don't specify the reference type
                 references.append(Reference(ref_key, 'book', **ref_data))
         return references
+
+    start_tag = '<span class="nocase">'
+    end_tag = '</span>'
+
+    def parse_string(self, string):
+        lower_string = string.lower()
+        end = 0
+        output = MixedString()
+        try:
+            while True:
+                start = lower_string.index(self.start_tag, end)
+                regular = string[end:start]
+                if regular:
+                    output += String(regular)
+                end = lower_string.index(self.end_tag, start + 1)
+                start += len(self.start_tag)
+                no_case = string[start:end]
+                output += NoCase(no_case)
+                end += len(self.end_tag)
+        except ValueError:
+            regular = string[end:]
+            if regular:
+                output += String(regular)
+        return output
 
     def parse_names(self, json_data):
         names = []
@@ -169,7 +196,6 @@ class ProcessorTest(object):
                 options[python_key] = value
 
         return CitationItem(reference_key, **options)
-
 
 
 def main():

citeproc/__init__.py
@@ -29,11 +29,12 @@ VARIABLES = (['abstract', 'annote', 'archive', 'archive_location',
               'citation_number', 'collection_title', 'container_title',
               'container_title_short', 'dimensions', 'DOI', 'event',
               'event_place', 'first_reference_note_number', 'genre', 'ISBN',
-              'ISSN', 'jurisdiction', 'keyword', 'locator', 'medium', 'note',
-              'original_publisher', 'original_publisher_place', 'original_title',
-              'page', 'page_first', 'PMCID', 'PMID', 'publisher',
-              'publisher_place', 'references', 'section', 'source', 'status',
-              'title', 'title_short', 'URL', 'version', 'year_suffix'] +
+              'ISSN', 'jurisdiction', 'keyword', 'language', 'locator',
+              'medium', 'note', 'original_publisher',
+              'original_publisher_place', 'original_title', 'page',
+              'page_first', 'PMCID', 'PMID', 'publisher', 'publisher_place',
+              'references', 'section', 'source', 'status', 'title',
+              'title_short', 'URL', 'version', 'year_suffix'] +
              NAMES + DATES + NUMBERS)
 
 LOCALES = [os.path.basename(path)[8:-4]

citeproc/formatter/html.py
@@ -59,7 +59,7 @@ class Bibliography(str):
     def __new__(cls, items):
         output = [cls.bib_prefix]
         for text in items:
-            text = cls.item_prefix + text + cls.item_suffix
+            text = cls.item_prefix + str(text) + cls.item_suffix
             output.append(text)
         output.append(cls.bib_suffix)
         return super().__new__(cls, '\n'.join(output))

commit 97609a744a7dcc063a627596420507d9adf61a7f

fix regressions introduced since 96ed838

citeproc/model.py
@@ -307,7 +307,12 @@ class Affixed(object):
 class Delimited(object):
     def join(self, strings, default_delimiter=''):
         delimiter = self.get('delimiter', default_delimiter)
-        return reduce(lambda a, b: a + delimiter + b, filter(None, strings))
+        try:
+            text = reduce(lambda a, b: a + delimiter + b,
+                          filter(lambda s: s is not None, strings))
+        except:
+            text = String('')
+        return text
 
 
 class Displayed(object):
@@ -440,8 +445,8 @@ class Sort(CitationStylesElement):
                         except AttributeError:
                             pass
                         try:
-                            left_key = int(str(left_key))
-                            right_key = int(str(right_key))
+                            left_key, right_key = (int(str(left_key)),
+                                                   int(str(right_key)))
                         except ValueError:
                             pass
                         result = (left_key > right_key) - (left_key < right_key)
@@ -490,7 +495,7 @@ class Key(CitationStylesElement):
             elif variable == 'citation_number':
                 sort_keys = [item.number for item in items]
             else:
-                sort_keys = [item.reference.get(variable) for item in items]
+                sort_keys = [item.get_field(variable) for item in items]
         elif 'macro' in self.attrib:
             layout = context.get_layout()
             # override name options
@@ -620,7 +625,7 @@ class Layout(CitationStylesElement, Parent, Formatted, Affixed, Delimited):
         output_items = []
         for item in citation_items:
             self.repressed = {}
-            text = self.render_children(item)
+            text = self.format(self.wrap(self.render_children(item)))
             if text is not None:
                 output_items.append(text)
         return self.get_target().Bibliography(output_items)
@@ -657,7 +662,7 @@ class Text(CitationStylesElement, Formatted, Affixed, Quoted, TextCased,
         elif 'term' in self.attrib:
             text = self._term(item)
         elif 'value' in self.attrib:
-            text = self.get('value')
+            text = String(self.preformat(self.get('value')))
 
         return text, language
 
@@ -678,7 +683,7 @@ class Text(CitationStylesElement, Formatted, Affixed, Quoted, TextCased,
             text = str(item.locator.identifier)
         elif variable == 'page-first' and variable not in item.reference:
             page = item.reference.page
-            text = Number.re_range.match(page).group(1)
+            text = Number.re_range.match(str(page)).group(1)
         else:
             text = item.reference[variable.replace('-', '_')]
 
@@ -1144,7 +1149,7 @@ class Name(CitationStylesElement, Formatted, Affixed, Delimited):
                         text = self.join([text, ''])
                 else:
                     text += ' '
-                text += ' {} '.format(and_term) + output[-1]
+                text += '{} '.format(and_term) + output[-1]
             else:
                 text = self.join(output, delimiter)
 

citeproc/string.py
@@ -10,6 +10,12 @@ class String(str):
     def __iadd__(self, other):
         return self.__add__(other)
 
+    def replace(self, *args, **kwargs):
+        return self.__class__(super().replace(*args, **kwargs))
+
+    def rstrip(self, *args, **kwargs):
+        return self.__class__(super().rstrip(*args, **kwargs))
+
     def lower(self):
         return self.__class__(super().lower())
 
@@ -53,6 +59,12 @@ class MixedString(list):
     def __getitem__(self, index):
         return str(self)[index]
 
+    def replace(self, *args):
+        return self.__class__([string.replace(*args) for string in self])
+
+    def translate(self, table):
+        return self.__class__([string.translate(table) for string in self])
+
     def lower(self):
         return self.__class__([string.lower() for string in self])
 
@@ -71,8 +83,14 @@ class MixedString(list):
     def isupper(self):
         return all(string.isupper() for string in self)
 
-    def split(self, sep=None, maxsplit=-1):
-        return str(self).split(sep, maxsplit)
+    def split(self, *args, **kwargs):
+        return str(self).split(*args, **kwargs)
+
+    def rstrip(self, *args, **kwargs):
+        rev_iter = reversed(self)
+        output = [next(rev_iter).rstrip(*args, **kwargs)]
+        output += [string for string in rev_iter]
+        return self.__class__(reversed(output))
 
     def words(self):
         for string in self:

tests/citeproc-test.py
@@ -70,6 +70,7 @@ class ProcessorTest(object):
     end_tag = '</span>'
 
     def parse_string(self, string):
+        string = str(string)
         lower_string = string.lower()
         end = 0
         output = MixedString()
@@ -252,7 +253,7 @@ def main():
             results = t.execute()
             results = reduce(lambda x, y: x+y,
                              [item.split('\n') for item in results])
-            results = [item.replace('&amp;', '&').replace('&', '&#38;')
+            results = [item.replace('&amp;', '&#38;')
                        for item in results]
             print('RES: ' + '\n     '.join(results), file=destination)
             if results == t.expected: