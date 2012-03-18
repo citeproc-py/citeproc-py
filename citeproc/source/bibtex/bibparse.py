5f579638939a0ceba98267a5f25bbfcbfbec58f0
Brecht Machiels <brecht@mos6581.org>
2012-03-18 18:34:28 +0100

    include license for bibparse.py

--- citeproc/source/bibtex/bibparse.py
+++ citeproc/source/bibtex/bibparse.py
@@ -1,27 +1,30 @@
-#!/usr/bin/python
+# Copyright (c) 2012, Vassilios Karakoidas (vassilios.karakoidas@gmail.com)
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#     * Redistributions of source code must retain the above copyright
+#       notice, this list of conditions and the following disclaimer.
+#     * Redistributions in binary form must reproduce the above copyright
+#       notice, this list of conditions and the following disclaimer in the
+#       documentation and/or other materials provided with the distribution.
+#     * Neither the name of the <organization> nor the
+#       names of its contributors may be used to endorse or promote products
+#       derived from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+# DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-#
-# Simple, naive bibtex parser
-#
-# Vassilios Karakoidas (2008) - vassilios.karakoidas@gmail.com
-#
-
-# bib categories
-#
-# @Article{
-# @Book{
-# @Booklet{
-# @InBook{
-# @InCollection{
-# @InProceedings{
-# @Manual{
-# @MastersThesis{
-# @Misc{
-# @PhDThesis{
-# @Proceedings{
-# @TechReport{
-# @Unpublished{
-#
+# based on bibparse.py from bibtex-py 0.8 by Vassilios Karakoidas
+# small changes by Brecht Machiels for citeproc-py
 
 import re
 import os
