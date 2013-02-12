183befef9a27f6ac75a9f7688918ed9e8ec9f2ca
Brecht Machiels <brecht@mos6581.org>
2013-02-12 20:57:26 +0100

    also add comments to the bibtex example

--- examples/bibtex.py
+++ examples/bibtex.py
@@ -1,16 +1,38 @@
 
-
-from citeproc import CitationStylesStyle, CitationStylesBibliography
-from citeproc import formatter
+# The references are parsed from a BibTeX database, so we import the
+# corresponding parser.
 from citeproc.source.bibtex import BibTeX
 
+# Import the citeproc-py classes we'll use below.
+from citeproc import CitationStylesStyle, CitationStylesBibliography
+from citeproc import formatter
 from citeproc import Citation, CitationItem
 
 
-bib_style = CitationStylesStyle('harvard1.csl')
+# Parse the BibTeX database.
+
 bib_source = BibTeX('xampl.bib')
+
+
+# load a CSL style (from the current directory)
+
+bib_style = CitationStylesStyle('harvard1.csl')
+
+
+# Create the citeproc-py bibliography, passing it the:
+# * CitationStylesStyle,
+# * BibliographySource (BibTeX in this case), and
+# * a formatter (plain, html, or you can write a custom formatter)
+
 bibliography = CitationStylesBibliography(bib_style, bib_source,
                                           formatter.plain)
+
+
+# Processing citations in a document need to be done in two passes as for some
+# CSL styles, a citation can depend on the order of citations in the
+# bibliography and thus on citations following the current one.
+# For this reason, we first need to register all citations with the
+# CitationStylesBibliography.
 
 citation1 = Citation([CitationItem('whole-collection')])
 citation2 = Citation([CitationItem('whole-set'), CitationItem('misc-full')])
@@ -21,9 +43,15 @@ bibliography.register(citation2)
 bibliography.register(citation3)
 
 
+# In the second pass, CitationStylesBibliography can generate citations.
+# CitationStylesBibliography.cite() requires a callback function to be passed
+# along to be called in case a CitationItem's key is not present in the
+# bilbiography.
+
 def warn(citation_item):
     print("WARNING: Reference with key '{}' not found in the bibliography."
           .format(citation_item.key))
+
 
 print('Citations')
 print('---------')
@@ -32,6 +60,8 @@ print(bibliography.cite(citation1, warn))
 print(bibliography.cite(citation2, warn))
 print(bibliography.cite(citation3, warn))
 
+
+# And finally, the bibliography can be rendered.
 
 print('')
 print('Bibliography')
