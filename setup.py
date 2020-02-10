b63bf60918338b397ef66afd85fd8f480b352007
Yaroslav Halchenko <debian@onerussian.com>
2020-02-09 19:08:21 -0500

    BF: added 3.7 and 3.8 to listed python versions in setup.py

--- setup.py
+++ setup.py
@@ -121,6 +121,8 @@ setup(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Topic :: Documentation',
         'Topic :: Printing',
         'Topic :: Software Development :: Documentation',
