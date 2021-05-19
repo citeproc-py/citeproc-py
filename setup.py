b92bb4a9862440879fa449145dfea572d66bbfea
Yaroslav Halchenko <debian@onerussian.com>
2021-05-19 18:58:32 -0400

    ENH: Add python 3.9 to supported
    And see if CI is still happy (last run 4 months ago) to release

--- setup.py
+++ setup.py
@@ -123,6 +123,7 @@ setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         'Topic :: Documentation',
         'Topic :: Printing',
         'Topic :: Software Development :: Documentation',
