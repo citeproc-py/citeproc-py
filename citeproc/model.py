2423d9bd0191d595b0a762f25b84abf5c5d95c17
Philippe Faist <phfaist@gmail.com>
2022-01-04 16:33:20 +0100

    fix bug?

--- citeproc/model.py
+++ citeproc/model.py
@@ -903,7 +903,7 @@ class Date_Part(CitationStylesElement, Formatted, Affixed, TextCased,
         except (AttributeError, IndexError):
             pass
 
-        if name == 'day':
+        if name == 'day' and date.day:
             form = self.get('form', 'numeric')
             if (form == 'ordinal'
                 and self.get_locale_option('limit-day-ordinals-to-day-1')
@@ -917,7 +917,7 @@ class Date_Part(CitationStylesElement, Formatted, Affixed, TextCased,
                 text = '{:02}'.format(date.day)
             elif form == 'ordinal':
                 text = to_ordinal(date.day, context)
-        elif name == 'month':
+        elif name == 'month' and date.month:
             form = self.get('form', 'long')
             strip_periods = self.get('form', False)
             try:
@@ -938,10 +938,10 @@ class Date_Part(CitationStylesElement, Formatted, Affixed, TextCased,
                     text = '{}'.format(index)
                 elif form == 'numeric-leading-zeros':
                     text = '{:02}'.format(index)
-        elif name == 'year':
+        elif name == 'year' and date.year:
             form = self.get('form', 'long')
             if form == 'long':
-                text = str(abs(int(date.year)))
+                text = str(abs(date.year))
                 if date.year < 0:
                     text += context.get_term('bc').single
                 elif date.year < 1000:
