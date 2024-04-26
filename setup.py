82854544be013735e793dab2c5a3cdb5756facf9
Tom Morrell <tmorrell@caltech.edu>
2024-04-26 13:13:15 -0700

    versioning: switch to versioneer

--- setup.py
+++ setup.py
@@ -14,44 +14,16 @@ from subprocess import Popen, PIPE
 from setuptools import setup, find_packages
 from setuptools.command.build_py import build_py
 from setuptools.command.develop import develop
-
+import versioneer
 
 PACKAGE = 'citeproc'
 BASE_PATH = os.path.dirname(os.path.abspath(__file__))
 PACKAGE_ABSPATH = os.path.join(BASE_PATH, PACKAGE)
 VERSION_FILE = os.path.join(PACKAGE_ABSPATH, 'version.py')
 
-VERSION_FORMAT = re.compile(r'v\d+\.\d+\.\d+')
 
 # All external commands are relative to BASE_PATH
 os.chdir(BASE_PATH)
-
-# retrieve the version number from git or VERSION_FILE
-# inspired by http://dcreager.net/2010/02/10/setuptools-git-version-numbers/
-try:
-    if os.path.lexists(os.path.join(BASE_PATH, 'PKG-INFO')):
-        raise OSError("PKG-INFO exists")
-    print('Attempting to get version number from git...')
-    git = Popen(['git', 'describe', '--always', '--dirty'],
-                stdout=PIPE, stderr=sys.stderr)
-    if git.wait() != 0:
-        raise OSError
-    line, = git.stdout.readlines()
-    line = line.strip().decode('ascii')
-    __version__ = line[1:] if VERSION_FORMAT.match(line) else line
-    # harmonize version a bit to Python PEP liking
-    # TODO: switch to use versioneer or versioningit
-    __version__ = __version__.replace('-', '.post', 1).replace('-', '+')
-    __release_date__ = datetime.now().strftime('%b %d %Y, %H:%M:%S')
-    with open(VERSION_FILE, 'w') as version_file:
-        version_file.write("__version__ = '{}'\n".format(__version__))
-        version_file.write("__release_date__ = '{}'\n".format(__release_date__))
-except OSError as e:
-    print(f'Assuming we are running from a source distribution: {e}')
-    # read version from VERSION_FILE
-    with open(VERSION_FILE) as version_file:
-        code = compile(version_file.read(), VERSION_FILE, 'exec')
-        exec(code)
 
 
 def long_description():
@@ -88,8 +60,8 @@ class custom_develop(develop):
 
 setup(
     name='citeproc-py',
-    version=__version__,
-    cmdclass = dict(build_py=custom_build_py, develop=custom_develop),
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass({'build_py': custom_build_py, 'develop': custom_develop}),
     packages=find_packages(),
     package_data={PACKAGE: ['data/locales/*.xml',
                             'data/locales/locales.json',
