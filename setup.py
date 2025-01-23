ec6cc9afb3d7f60235452d1c0418b5b954298fcc
Yaroslav Halchenko <debian@onerussian.com>
2025-01-23 08:46:58 -0500

    Add Python 3.13 into supported/tested

--- setup.py
+++ setup.py
@@ -99,6 +99,7 @@ setup(
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
         'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3.13',
         'Topic :: Documentation',
         'Topic :: Printing',
         'Topic :: Software Development :: Documentation',
